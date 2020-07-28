32b5a02f6f79a2eec83c4ebd3bfc628329392731
pnuii <phothongd@gmail.com>
2020-07-28 16:02:11 +0700

    01 function created

--- 01.c
+++ 01.c
@@ -1,10 +1,33 @@
 #include<stdio.h>
 
+//Upgrade 
+//01
+
 void main()
 {
-	char *p = "Test string";
+	char *a, *b, *c, *d, *e, *f, *g, *h, *i, *j;
+	a = "\"This is C Programming\"\n";
+	b = "\'I\'\n";
+	c = "\'L\'\n";
+	d = "\'O\'\n";
+	e = "\'V\'\n";
+	f = "\'E\'\n";
+	g = "\"It very much\"\n";
+	h = "\"\\n is new line\"\n";
+	i = "\"\\t is tab\"\n";
+	j = "\"\\a is bell sound\"\n";
+
+	printf(a);
+	printf(b);
+	printf(c);
+	printf(d);
+	printf(e);
+	printf(f);
+	printf(g);
+	printf(h);
+	printf(i);
+	printf(j);
 	
-	//strcat()
-	
-	printf(p);
+	getch(); //youd sua khao
 }
+
